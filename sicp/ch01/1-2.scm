b3f6b155cd9400ff7dc6f870cdc639e0658498f8
0xd3 <1564447846@qq.com>
2016-06-02 23:43:37 +0800

    complete sicp 1.2

--- sicp/ch01/1-2.scm
+++ sicp/ch01/1-2.scm
@@ -27,7 +27,7 @@
         ((devides? test-divisor n) test-divisor)
         (else (find-divisor n (+ test-divisor 1)))))
 
-(define (divides? a b)
+(define (devides? a b)
   (= (remainder b a ) 0))
 
 (define (prime? n)
@@ -51,3 +51,67 @@
         (else
           (remainder (* base (expmod base (- exp 1) m))
                      m))))
+
+(define (square a)
+  (* a a))
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime) start-time))(newline)))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapesd-time))
+
+(define (next-odd n)
+  (if (odd? n)
+    (+ n 2)
+    (+ n 1)))
+
+(define (continue-primes n count)
+  (cond ((= count 0)
+         (display "are primes."))
+        ((prime? n)
+         (display n)
+         (newline)
+         (coutinue-primes (next-odd n) (- cout 1)))
+        (else
+          (continue-primes (next-odd n) count))))
+
+(define (search-for-primes n)
+  (let((start-time (real-time-clock)))
+    (continue-primes n 3)
+    (- (real-time-clock) start-time)))
+
+;; 1.27
+(define (carmichael-test n)
+  (test-iter 1 n))
+
+(define (test-iter a n)
+  (cond ((= a n)
+         #t)
+        ((congruent? a n)
+         (test-iter (+ a 1) n))
+        (else
+          #f)))
+
+(define (congruent? a n)
+  (= (expmod a n n) a))
+
+;; 1.28
+(define (expmod base exp m)
+  (cond ((= exp 0)
+         1)
+        ((nontrivial-square-root? base m)
+         0)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m)
+         (else
+           (remainder (* base (expmod base (- exp 1) m))
+                      m)))))
