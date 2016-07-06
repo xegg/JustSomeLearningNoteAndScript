f116879d47c7696d7469ec250c255988d5acabf5
0xd3 <1564447846@qq.com>
2016-07-06 22:03:06 +0800

    add_binaray algorithm

---
+++ algorithm/leetcode/kth_largest_element_in_an_array.cpp
@@ -0,0 +1,35 @@
+
+int findKthLargest_buildin(vector<int>& nums, int k) {
+    int n=nums.size();
+    std::nth_element(nums.begin(),nums.end()-k,nums.end());
+    return nums[n-k];
+}
+
+int partition(vector<int>& nums, int left, int right) {
+    int pivot = nums[left];
+    int l = left + 1, r = right;
+    while (l <= r) {
+        if (nums[l] < pivot && nums[r] > pivot){
+            swap(nums[l++], nums[r--]);
+        }
+        if (nums[l] >= pivot) l++;
+        if (nums[r] <= pivot) r--;
+    }
+    swap(nums[left], nums[r]);
+    return r;
+}
+
+int findKthLargest_qsort(vector<int>& nums, int k) {
+    int left = 0, right = nums.size() - 1;
+    while (true) {
+        int pos = partition(nums, left, right);
+        if (pos == k - 1){
+            return nums[pos];
+        }
+        if (pos > k - 1) {
+            right = pos - 1;
+        }else{
+            left = pos + 1;
+        }
+    }
+}
