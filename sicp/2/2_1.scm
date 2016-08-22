28387a0e0ab192833f6dc673356f7b4447ddcb57
0xd3 <1564447846@qq.com>
2016-08-23 01:27:24 +0800

    algorithm and sicp

---
+++ sicp/2/2_1.scm
@@ -0,0 +1,244 @@
+#lang sicp
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom y)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+    (make-rat (- (* (numer x) (denom y))
+                 (* (numer y) (denom x)))
+              (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+    (make-rat (* (numer x) (numer y))
+              (* (denom x) (denom y))))
+
+(define (div-rat x y)
+    (make-rat (* (numer x) (denom y))
+              (* (denom x) (numer y))))
+
+(define (equal-rat? x y)
+    (= (* (numer x) (denom y))
+       (* (numer y) (denom x))))
+
+
+
+(define (make-rat n d) (cons n d))
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+    (newline)
+    (display (numer x))
+    (display "/")
+    (display (denom x)))
+
+
+(define (gcd a b)
+  ( if (= b 0)
+       a
+       (gcd b (remainder a b))))
+
+(define (make-rat1 n d)
+    (let ((g (gcd n d)))
+         (cons (/ n g) (/ d g))))
+
+(define one-half (make-rat 1 2))
+(define one-third (make-rat 1 3))
+
+;; 2.1
+(define (make-rat2 n d)
+    (if (< d 0)
+        (cons (- n) (- d))
+        (cons n d)))
+
+(define (numer1 x)
+  (let ((g (gcd (car x) (cdr x))))
+    (/ (car x) g)))
+
+(define (denom1 x)
+  (let ((g (gcd (car x) (cdr x))))
+    (/ (cdr x) g)))
+
+;;2.2
+(define (make-segment start-point end-point)
+    (cons start-point end-point))
+
+(define (start-segment seg)
+    (car seg))
+
+(define (end-segment seg)
+    (cdr seg))
+
+(define (make-point x y)
+    (cons x y))
+
+(define (x-point p)
+    (car p))
+
+(define (y-point p)
+    (cdr p))
+
+(define (midpoint-segment seg)
+    (let ((start (start-segment seg))
+          (end (end-segment seg)))
+        (make-point (average (x-point start)
+                             (x-point end))
+                    (average (y-point start)
+                             (y-point end)))))
+
+(define (average x y)
+    (/ (+ x y)
+       2.0))
+
+(define (print-point p)
+    (newline)
+    (display "(")
+    (display (x-point p))
+    (display ",")
+    (display (y-point p))
+    (display ")"))
+
+;; 2.3
+(define (perimeter-rectangle r)
+    (let ((length (length-of-rectangle r))
+          (width (width-of-rectangle r)))
+        (* 2 
+           (+ length width))))
+
+(define (area-rectangle r)
+    (* (length-of-rectangle r)
+       (width-of-rectangle r)))
+
+
+(define (make-rectangle length-1 length-2 width-1 width-2)
+    (cons (cons length-1 length-2)
+          (cons width-1 width-2)))
+
+
+(define (length-1-rectangle r)
+    (car (car r)))
+
+(define (length-2-rectangle r)
+    (cdr (car r)))
+
+(define (width-1-rectangle r)
+    (car (cdr r)))
+
+(define (width-2-rectangle r)
+    (cdr (cdr r)))
+
+(define (print-rectangle r)
+    (let ((l1 (length-1-rectangle r))
+          (l2 (length-2-rectangle r))
+          (w1 (width-1-rectangle r))
+          (w2 (width-2-rectangle r)))
+
+        (newline)
+        (display "Length 1:")
+        (print-point (start-segment l1))
+        (print-point (end-segment l1))
+
+        (newline)
+        (display "Length 2:")
+        (print-point (start-segment l2))
+        (print-point (end-segment l2))
+
+        (newline)
+        (display "Width 1:")
+        (print-point (start-segment w1))
+        (print-point (end-segment w1))
+
+        (newline)
+        (display "Width 2:")
+        (print-point (start-segment w2))
+        (print-point (end-segment w2))))
+
+(define (length-of-rectangle r)
+    (let ((length (length-1-rectangle r)))
+        (let ((start (start-segment length))
+              (end (end-segment length)))
+            (- (x-point end)
+               (x-point start)))))
+
+(define (width-of-rectangle r)
+    (let ((width (width-1-rectangle r)))
+        (let ((start (start-segment width))
+              (end (end-segment width)))
+            (- (y-point end)
+               (y-point start)))))
+
+(define (make-rectangle1 length width)
+    (cons length width))
+
+(define (length-rectangle r)
+    (car r))
+
+(define (width-rectangle r)
+    (cdr r))
+
+(define (length-of-rectangle1 r)
+    (let ((length (length-rectangle r)))
+        (let ((start (start-segment length))
+              (end (end-segment length)))
+            (- (x-point end)
+               (x-point start)))))
+
+(define (width-of-rectangle1 r)
+    (let ((width (width-rectangle r)))
+        (let ((start (start-segment width))
+              (end (end-segment width)))
+            (- (y-point end)
+               (y-point start)))))
+;;;
+(define (cons1 x y)
+    (define (dispatch m)
+        (cond ((= m 0) x)
+              ((= m 1) y)))
+     dispatch)
+(define (car z) (z 0))
+(define (cdr z) (z 1))
+
+
+;; 2.4
+(define (cdr1 z)
+    (z (lambda (p q) q)))
+
+;; 2.5
+(define (cons2 x y)
+    (* (expt 2 x)
+       (expt 3 y)))
+
+
+(define (car2 z)
+    (if (= 0 (remainder z 2))
+        (+ 1 (car (/ z 2)))
+        0))
+
+(define (cdr2 z)
+    (if (= 0 (remainder z 3))
+        (+ 1 (cdr (/ z 3)))
+        0))
+
+;; 2.6
+(define zero
+  (lambda (f) (lambda (x) x)))
+
+(define (one-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
+
+;;(add-1 zero)
+;;(add-1 (lambda (f) (lambda (x) x)))
+;;(lambda (f) (lambda (x) (f (((lambda (f) (lambda (x) x)) f) x))))
+;;(lambda (f) (lambda (x) (f ((lambda (x) x) x))))
+;;(lambda (f) (lambda (x) (f x)))
+
+(define one (lambda (f) (lambda (x) (f x))))
+
+(define +
+    (lambda (m)
+        (lambda (n)
+            (lambda (f)
+                (lambda (x)
+                    (m f (n f x)))))))
