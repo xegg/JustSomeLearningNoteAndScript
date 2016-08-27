a46b1bc25ae1d2714e89f66186b61313023e50f5
0xd3 <1564447846@qq.com>
2016-08-28 00:16:41 +0800

    TFwH 3

--- haskell/thinking_functionally_with_haskell/2/ex.hs
+++ haskell/thinking_functionally_with_haskell/2/ex.hs
@@ -26,3 +26,30 @@ suffix d = if d == 1 || d==21 || d==31 then "st" else
     "th"
 
 months = ['Jun']
+
+
+type CIN = [Char]
+
+addSum :: CIN -> CIN
+addSum cin =
+  cin ++ show (n `div` 10) ++ show (n `mod` 10)
+  where n = sum (map fromDigit cin)
+
+valid :: CIN -> Bool
+valid cin = cin == addSum (take 8 cin)
+
+fromDigit :: Char -> Int
+fromDigit c = read [c]
+
+import Data.Char (toLower,isAlpha)
+
+palindrome :: IO()
+palindrome 
+ = do {putStrLn "Enter a string:";
+       xs <- getLine;
+       if isPalindrome xs then putStrLn "Yes!"
+       else putStrLn "No!"}
+
+isPalindrome :: String -> Bool
+isPalindrome xs = (ys == reverse ys)
+  where ys = map toLower (filter isAlpha xs)
