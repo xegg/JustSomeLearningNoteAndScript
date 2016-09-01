1333706ed50aad489166f724a82c2dfb45e34976
0xd3 <1564447846@qq.com>
2016-09-02 00:31:32 +0800

    sicp 2.2.3

--- sicp/2/2_2.scm
+++ sicp/2/2_2.scm
@@ -1,4 +1,5 @@
 #lang sicp
+
 (define (list-ref items n)
   (if (= n 0)
     (car items)
@@ -189,10 +190,10 @@
         (else (cons (scale-tree (car tree) factor)
                     (scale-tree (cdr tree) factor)))))
 
-(define (scale-tree tree factor)
+(define (scale-tree1 tree factor)
   (map (lambda (sub-tree)
          (if (pair? sub-tree)
-           (scal-tree sub-tree factor)
+           (scale-tree sub-tree factor)
            (* sub-tree factor)))
          tree))
 
@@ -206,7 +207,7 @@
             (cons (square-tree (car tree))
                   (square-tree (cdr tree))))))
 
-(define (square-tree tree)
+(define (square-tree1 tree)
     (map (lambda (sub-tree)
              (if (pair? sub-tree)           ; 如果有左右子树
                  (square-tree sub-tree)     ; 那么递归地处理它们
@@ -230,3 +231,269 @@
             (append rest (map (lambda (x)
                                 (cons (car s) x))
                               rest)))))
+
+
+(define (sum-odd-squares tree)
+  (cond ((null? tree) 0)
+        ((not (pair? tree))
+         (if (odd? tree) (square tree) 0))
+        (else (+ (sum-odd-squares (car tree))
+                 (sum-odd-squares (cdr tree))))))
+
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        ((= n 2) 1)
+        (else (+ (fib (- n 1))
+                 (fib (- n 2))))))
+
+(define (even-fibs n)
+  (define (next k)
+    (if (> k n)
+        nil
+        (let ((f (fib k)))
+          (if (even? f)
+              (cons f (next (+ k 1)))
+              (next (+ k 1))))))
+    (next 0))
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (enumerate-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (list tree))
+        (else (append (enumerate-tree (car tree))
+                      (enumerate-tree (cdr tree))))))
+
+(define (sum-odd-squares2 tree)
+  (accumulate +
+              0
+              (map square
+                   (filter odd?
+                           (enumerate-tree tree)))))
+
+
+(define (even-fibs1 n)
+  (accumulate cons
+              nil
+              (filter even?
+                      (map fib
+                           (enumerate-interval 0 n)))))
+
+(define (list-fib-squares n)
+  (accumulate cons
+              nil
+              (map square
+                   (map fib
+                        (enumerate-interval 0 n)))))
+
+;; 2.34
+(define (horner-eval x coefficient-sequence)
+    (accumulate (lambda (this-coeff higher-terms)
+                    (+ this-coeff (* x higher-terms)))
+                0
+                coefficient-sequence))
+
+;; 2.35
+
+(define (count-leaves1 tree)
+    (accumulate +
+                0
+                (map (lambda (sub-tree)
+                         (if (pair? sub-tree)           ; 如果这个节点有分支
+                             (count-leaves sub-tree)    ; 那么这个节点调用 count-leaves 的结果就是这个节点的树叶数量
+                             1))                        ; 遇上一个叶子节点就返回 1
+                     tree)))
+
+;; 2.36
+(define (car-n seqs)
+    (map car seqs))
+
+(define (cdr-n seqs)
+    (map cdr seqs))
+
+(define (accumulate-n op init seqs)
+    (if (null? (car seqs))
+        '()
+        (cons (accumulate op init (car-n seqs))
+              (accumulate-n op init (cdr-n seqs)))))
+
+
+;; 2.37
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+
+(define (matrix-*-vector m v)
+    (map (lambda (col)
+             (dot-product col v))
+         m))
+
+(define (transpose m)
+    (accumulate-n cons '() m))
+
+
+(define (matrix-*-matrix m n)
+    (let ((cols (transpose n)))
+        (map (lambda (col-of-m)
+                 (map (lambda (col-of-cols)
+                          (dot-product col-of-m 
+                                       col-of-cols))
+                      cols))
+             m)))
+
+(define (matrix-*-matrix1 m n)
+    (let ((trans-n (transpose n)))
+        (map (lambda (col-of-m)
+                (matrix-*-vector trans-n col-of-m))
+                m)))
+
+;; 2.38
+(define (fold-left op initial sequence)
+    (define (iter result rest)
+        (if (null? rest)
+            result
+            (iter (op result (car rest))
+                  (cdr rest))))
+    (iter initial sequence))
+
+(define fold-right accumulate)
+
+
+
+;;
+(define (smallest-devisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((devides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (devides? a b)
+  (= (remainder b a ) 0))
+
+(define (prime? n)
+  (= n (smallest-devisor n)))
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum?
+               (flatmap
+                (lambda (i)
+                  (map (lambda (j) (list i j))
+                       (enumerate-interval 1 (- i 1))))
+                  (enumerate-interval 1 n)))))
+
+(define (permutations s)
+  (if (null? s)
+      (list nil)
+      (flatmap (lambda (x)
+                 (map (lambda (p) (cons x p))
+                      (permutations (remove x s))))
+               s)))
+
+(define (remove item sequence)
+  (filter (lambda (x) (not (= x item)))
+          sequence))
+
+;; 2.40
+
+(define (unique-pairs n)
+    (flatmap (lambda (i)
+                 (map (lambda (j) (list i j))
+                      (enumerate-interval 1 (- i 1))))
+             (enumerate-interval 1 n)))
+
+(define (prime-sum-pairs1 n)
+    (map make-pair-sum
+         (filter prime-sum? (unique-pairs n))))
+
+
+;; 2.41
+
+(define (unique-triples n)
+    (flatmap (lambda (i)
+                 (map (lambda (j)                   ; cons 起 i 元素和二元组 j ,组成三元组
+                          (cons i j))
+                      (unique-pairs (- i 1))))      ; 生成不大于 i 的所有相异整数二元组
+             (enumerate-interval 1 n)))             ; 生成 1 至 n 的所有整数，作为 i
+
+
+(define (triple-sum-equal-to? sum triple)
+    (= sum
+       (fold-right + 0 triple)))
+
+(define (remove-triples-not-equal-to sum triple)
+    (filter (lambda (current-triple)
+                (triple-sum-equal-to? sum current-triple))
+            triple))
+
+;; 2.42
+(define (queens board-size)
+  (define (queen-cols k)
+  (if (= k 0)
+      (list empty-board)
+      (filter
+       (lambda (positions) (safe? k positions))
+       (flatmap
+        (lambda (rest-of-queens)
+                (map (lambda (new-row)
+                       (adjoin-position new-row k rest-of-queens))
+                     (enumerate-interval 1 board-size)))
+       (queen-cols (- k 1))))))
+  (queen-cols board-size))
+
+
+(define empty-board '())
+
+(define (safe? k position)
+    (iter-check (car position) 
+                (cdr position)
+                 1))
+
+(define (adjoin-position new-row k rest-of-queens)
+    (cons new-row rest-of-queens))
+
+(define (iter-check row-of-new-queen rest-of-queens i)
+    (if (null? rest-of-queens)  ; 下方所有皇后检查完毕,新皇后安全
+        #t
+        (let ((row-of-current-queen (car rest-of-queens)))
+            (if (or (= row-of-new-queen row-of-current-queen)           ; 行碰撞
+                    (= row-of-new-queen (+ i row-of-current-queen))     ; 右下方碰撞
+                    (= row-of-new-queen (- row-of-current-queen i)))    ; 左下方碰撞
+                #f
+                (iter-check row-of-new-queen 
+                            (cdr rest-of-queens)    ; 继续检查剩余的皇后
+                            (+ i 1))))))            ; 更新步进值
+
+;;(for-each (lambda (pos)
+;;                    (begin
+;;                        (display pos)
+;;                       (newline)))
+;;                (queens 8))
