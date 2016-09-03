8ad12824b0808848f15d4127ded924b15c588603
0xd3 <1564447846@qq.com>
2016-09-04 00:36:06 +0800

    algorithm

--- algorithm/beauty_of_code/2_7.cpp
+++ algorithm/beauty_of_code/2_7.cpp
@@ -2,3 +2,36 @@ int gcd(int x, int y) {
     return (!y) ? x gcd(y, x % y)
 }
 
+
+BigInt gcd(BigInt x, BigInt y)
+{
+    if (x < y)
+        return gcd(y, x);
+    if (y == 0)
+        return x;
+    else
+        return gcd(x-y, y);
+}
+
+BigInt gcd(BigInt x, BigInt y) {
+    if (x < y)
+        return gcd(y, x);
+    if (y == 0)
+        return x;
+    else {
+        if(IsEven(x)) {
+            if(IsEven(y))
+                return (gcd(x >> 1, y >> 1) << 1);
+            else {
+                return gcd(x >> 1, y);
+            }
+        }
+        else {
+            if(IsEven(y))
+                return gcd(x, y >> 1);
+            else {
+                return gcd(y, x - y);
+            }
+        }
+    }
+}
