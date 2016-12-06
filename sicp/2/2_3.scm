c8b605ddb1ad1326803560feb7d797f806ded4f2
0xd3 <1564447846@qq.com>
2016-12-07 00:53:02 +0800

    sicp: 2.2.3

--- sicp/2/2_3.scm
+++ sicp/2/2_3.scm
@@ -19,9 +19,11 @@
 
 ;;; 54-equal.scm
 
-(define (equal? x y)
+(define (equal?2 x y)
     (cond ((and (symbol? x) (symbol? y))
             (symbol-equal? x y))
+          ((and (list? x) (list? y))
+            (list-equal? x y))
           ((and (list? x) (list? y))
             (list-equal? x y))
           (else
@@ -147,6 +149,7 @@
 
 
 ;; 2.57
+(define single-operand? 1)
 (define (make-sum2 a1 . a2)
     (if (single-operand? a2)
         (let ((a2 (car a2)))
@@ -215,18 +218,18 @@
           (else
             (list a1 '+ a2))))              ; 修改
 
-(define (sum? x)
+(define (sum?3 x)
     (and (pair? x)
          (eq? (cadr x) '+)))                ; 修改
 
-(define (addend s)
+(define (addend3 s)
     (car s))                                ; 修改
 
-(define (augend s)
+(define (augend3 s)
     (caddr s))
 
 
-(define (make-product m1 m2)
+(define (make-product3 m1 m2)
     (cond ((or (=number? m1 0) (=number? m2 0))
             0)
           ((=number? m1 1)
@@ -238,12 +241,277 @@
           (else
             (list m1 '* m2))))              ; 修改
 
-(define (product? x)
+(define (product?3 x)
     (and (pair? x)
          (eq? (cadr x) '*)))                ; 修改
 
-(define (multiplier p)
+(define (multiplie3r p)
     (car p))                                ; 修改
 
-(define (multiplicand p)
+(define (multiplicand3 p)
     (caddr p))
+
+
+;; 2.3.3
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1)
+               (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
+
+
+
+(define (union-set set1 set2)
+    (iter (append set1 set2) '()))
+
+(define (iter input result)
+    (if (null? input)
+        (reverse result)
+        (let ((current-element (car input))
+              (remain-element (cdr input)))
+            (if (element-of-set? current-element result)
+                (iter remain-element result)
+                (iter remain-element
+                      (cons current-element result))))))
+
+;; 2.60
+(define (adjoin-set2 x set)
+    (cons x set))
+
+(define (intersection-set2 set another)
+    (define (iter set result)
+        (if (or (null? set) (null? another))
+            (reverse result)
+            (let ((current-element (car set))
+                  (remain-element (cdr set)))
+                (if (and (element-of-set? current-element another)
+                         (not (element-of-set? current-element result)))
+                    (iter remain-element
+                          (cons current-element result))
+                    (iter remain-element result)))))
+    (iter set '()))
+
+;; if set is order
+(define (intersection-set3 set1 set2)
+  (if (or (null? set1) (null? set2))
+      '()
+      (let ((x1 (car set1)) (x2 (car set2)))
+        (cond ((= x1 x2)
+               (cons x1
+                     (intersection-set (cdr set1)
+                                       (cdr set2))))
+              ((< x1 x2)
+               (intersection-set (cdr set1) set2))
+              ((< x2 x1)
+               (intersection-set set1 (cdr set2)))))))
+;; 2.61
+(define (adjoin-set3 x set)
+    (if (null? set)
+        (list x)
+        (let ((current-element (car set))
+              (remain-element (cdr set)))
+            (cond ((= x current-element)
+                    set)
+                  ((> x current-element)
+                    (cons current-element
+                          (adjoin-set x remain-element)))
+                  ((< x current-element)
+                    (cons x set))))))
+
+;; 2.62
+(define (union-set2 set another)
+    (cond ((and (null? set) (null? another))
+            '())
+          ((null? set)
+            another)
+          ((null? another)
+            set)
+          (else
+            (let ((x (car set)) (y (car another)))
+                (cond ((= x y)
+                        (cons x (union-set (cdr set) (cdr another))))
+                      ((< x y)
+                        (cons x (union-set (cdr set) another)))
+                      ((> x y)
+                        (cons y (union-set set (cdr another)))))))))
+
+
+;; 2.63
+(define (tree->list-1 tree)
+    (if (null? tree)
+        '()
+        (append (tree->list-1 (left-branch tree))
+                (cons (entry tree)
+                      (tree->list-1 (right-branch tree))))))
+
+(define (tree->list-2 tree)
+    (define (copy-to-list tree result-list)
+        (if (null? tree)
+            result-list
+            (copy-to-list (left-branch tree)
+                          (cons (entry tree)
+                                (copy-to-list (right-branch tree)
+                                              result-list)))))
+    (copy-to-list tree '()))
+
+(define (entry tree) (car tree))
+
+(define (left-branch tree) (cadr tree))
+
+(define (right-branch tree) (caddr tree))
+
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (element-of-set?4 x set)
+  (cond ((null? set) false)
+        ((= x (entry set)) true)
+        ((< x (entry set))
+         (element-of-set? x (left-branch set)))
+        ((> x (entry set))
+         (element-of-set? x (right-branch set)))))
+
+(define (adjoin-set4 x set)
+  (cond ((null? set) (make-tree x '() '()))
+        ((= x (entry set)) set)
+        ((< x (entry set))
+         (make-tree (entry set) 
+                    (adjoin-set4 x (left-branch set))
+                    (right-branch set)))
+        ((> x (entry set))
+         (make-tree (entry set)
+                    (left-branch set)
+                    (adjoin-set4 x (right-branch set))))))
+
+;; 2.64
+(define (list->tree elements)
+    (car (partial-tree elements (length elements))))
+
+(define (partial-tree elts n)
+    (if (= n 0)
+        (cons '() elts)
+        (let ((left-size (quotient (- n 1) 2)))
+            (let ((left-result (partial-tree elts left-size)))
+                (let ((left-tree (car left-result))
+                      (non-left-elts (cdr left-result))
+                      (right-size (- n (+ left-size 1))))
+                    (let ((this-entry (car non-left-elts))
+                          (right-result (partial-tree (cdr non-left-elts)
+                                                      right-size)))
+                        (let ((right-tree (car right-result))
+                              (remaining-elts (cdr right-result)))
+                            (cons (make-tree this-entry left-tree right-tree)
+                                  remaining-elts))))))))
+
+;; 2.65
+(define (intersection-tree tree another)
+    (list->tree
+        (intersection-set (tree->list-2 tree)
+                          (tree->list-2 another))))
+
+
+(define (union-tree tree another)
+    (list->tree
+        (union-set (tree->list-2 tree)
+                   (tree->list-2 another))))
+
+;;; 66-lookup.scm
+(define key 1)
+(define (lookup given-key tree-of-records)
+    (if (null? tree-of-records)                                             ; 数据库为空，查找失败
+        #f
+        (let ((entry-key (key (entry tree-of-records))))                    ; 获取当前节点的键
+            (cond ((= given-key entry-key)                                  ; 对比当前节点的键和给定的查找键
+                    (entry tree-of-records))                                ; 决定查找的方向
+                  ((> given-key entry-key)
+                    (lookup given-key (right-branch tree-of-records)))
+                  ((< given-key entry-key)
+                    (lookup given-key (left-branch tree-of-records)))))))
+
+
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+
+(define (symbol-leaf x) (cadr x))
+(define (weight-leaf x) (caddr x))
+
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))))
+
+
+(define (left-branch2 tree) (car tree))
+(define (right-branch2 tree) (cadr tree))
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+        '()
+        (let ((next-branch
+               (choose-branch (car bits) current-branch)))
+          (if (leaf? next-branch)
+              (cons (symbol-leaf next-branch)
+                    (decode-1 (cdr bits) tree))
+              (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch2 branch))
+        ((= bit 1) (right-branch2 branch))
+        (else (display "bad bit -- CHOOSE_BRANCH" bit))))
+
+(define (adjoin-set5 x set)
+  (cond ((null? set) (list x))
+        ((< (weight x) (weight (car set))) (cons x set))
+        (else (cons (car set)
+                    (adjoin-set5 x (cdr set))))))
+
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree
+                   (make-leaf 'B 2)
+                   (make-code-tree (make-leaf 'D 1)
+                                   (make-leaf 'C 1)))))
+                                                        
+(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+
+
+;; 2.67
+;;(decode sample-message sample-tree)
+
+;; 2.68
+(define (encode message tree)
+  (if (null? message)
+      '()
+      (append (encode-symbol (car message) tree)
+              (encode (cdr message) tree))))
+
+(define (encode-symbol x) x)
+
+
