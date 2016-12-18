a83e6434662b13b1ceea4cb01a1c2c8e59d70c92
0xd3 <1564447846@qq.com>
2016-12-18 13:30:32 +0800

    sicp: finish chapter 2-4

--- sicp/2/2_4.scm
+++ sicp/2/2_4.scm
@@ -283,3 +283,44 @@
 ;数据导向：数据导向可以很方便地通过包机制增加新类型和新的通用操作，因此无论是增加新类型还是增加新操作，这种策略都很适合。
 
 ;消息传递：消息传递将数据对象和数据对象所需的操作整合在一起，因此它可以很方便地增加新类型，但是这种策略不适合增加新操作，因为每次为某个数据对象增加新操作之后，这个数据对象已有的实例全部都要重新实例化才能使用新操作。
+
+;; 2.73
+(define (install-sum-package)
+
+    ;;; internal procedures 
+    (define (addend s)
+        (car s))
+
+    (define (augend s)
+        (cadr s))
+
+    (define (make-sum x y)
+        (cond ((=number? x 0)
+                y)
+              ((=number? y 0)
+                x)
+              ((and (number? x) (number? y))
+                (+ x y))
+              (else
+                (attach-tag '+ x y))))
+
+    ;;; interface to the rest of the system
+    (put 'addend '+ addend)
+    (put 'augend '+ augend)
+    (put 'make-sum '+ make-sum)
+
+    (put 'deriv '+
+        (lambda (exp var)
+            (make-sum (deriv (addend exp) var)
+                      (deriv (augend exp) var))))
+
+'done)
+
+(define (make-sum x y)
+    ((get 'make-sum '+) x y))
+
+(define (addend sum)
+    ((get 'addend '+) (contents sum)))
+
+(define (augend sum)
+    ((get 'augend '+) (contents sum)))
