c3b5c8721e2da267918cb8ee80a1429f7fbf1cac
0xd3 <1564447846@qq.com>
2017-03-16 21:49:27 +0800

    feat: go pipline example

---
+++ go/pipeline.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"github.com/songgao/go.pipeline"
+)
+
+func main() {
+	counter := 0
+
+	pipeline.NewPipeline().
+		C("ls", "-h", "-l", "/bin").C("egrep", "--color=always", "-e", `\ *[0-9.]*M\ *`).
+		L(func(in string) string {
+			counter++
+			return fmt.Sprintf("%d:\t%s", counter, in)
+		}, nil).P()
+}
